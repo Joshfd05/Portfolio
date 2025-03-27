78a128248cee89549c4a6cc7a7421730d45b7480
Joshua <fernandojoshua102@gmail.com>
2025-03-27 18:33:11 +0530

    Finished

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,21 +1,13 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-footer',
-//   imports: [],
-//   templateUrl: './footer.component.html',
-//   styleUrl: './footer.component.css'
-// })
-// export class FooterComponent {
-
-// }
-
-
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
+  imports: [],
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrl: './footer.component.css'
 })
-export class FooterComponent {}
+export class FooterComponent {
+
+}
+
+
